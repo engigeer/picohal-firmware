87b28a1232e482a9f17841fec418d46e5b810ecd
Mitchell Grams <mitchell.grams@apollomachine.com>
2025-03-26 10:30:11 -0600

    swap mist + flood relays

--- coolant_control.py
+++ coolant_control.py
@@ -5,10 +5,10 @@ def update_coolant_pins():
     from modbus_registers import client
     #only update the pins if they were assigned.
     if(argon) :
-        argon.value(client.get_hreg(0x100) & 1)
+        argon.value(client.get_hreg(0x100) & 2)
     
     if(powder1_state) :
-        powder1_state.value(client.get_hreg(0x100) & 2)
+        powder1_state.value(client.get_hreg(0x100) & 1)
 
 
 def set_coolant_callback(reg_type, address, val):

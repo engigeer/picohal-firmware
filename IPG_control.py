6ed88c1d63d7e62fdd2fe2016474dc07deec01d4
Mitchell Grams <mitchell.grams@apollomachine.com>
2025-03-31 11:42:17 -0600

    Add laser linearization function

--- IPG_control.py
+++ IPG_control.py
@@ -80,14 +80,32 @@ def update_IPG_pins():
         laser_guide_on = False
         sendcmd("cmd=rerr")
 
+def linearize_power_output(rpm):
+    linear_data = [
+        {"slope": 8.463984e-01, "offset": -2.765132e+02, "rpm": 854.8},   # Segment 1
+        {"slope": 8.383298e-01, "offset": -2.834102e+02, "rpm": 2047.6},  # Segment 2
+        {"slope": 8.715443e-01, "offset": -2.847283e+02, "rpm": 3195.0},  # Segment 3
+        {"slope": 8.715443e-01, "offset": -2.847283e+02, "rpm": 4210.5},   # Segment 4
+    ]
+
+    if not rpm > 0:
+       return 0
+    
+    idx = len(linear_data)-1
+    while idx > 0 and rpm < linear_data[idx]["rpm"]:
+        idx -= 1
+
+    power_out = max(0, min(4000, int(linear_data[idx]["slope"] * rpm - linear_data[idx]["offset"]))) // 40
+    
+    return power_out
+
 def update_IPG_laser_power():
     from modbus_registers import client
 
     rpm_setpoint = client.get_hreg(0x201)  # RPM as unit16
 
     # Send power setpoint
-    rpm_limit = max(0, min(4000, int(rpm_setpoint)))
-    laser_power_value = rpm_limit // 40
+    laser_power_value = linearize_power_output(rpm_setpoint)
     sendcmd(f"ver=1&sdc={laser_power_value}")
 
 def update_IPG_laser_state():

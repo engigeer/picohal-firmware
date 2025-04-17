a2570c3183106e0c9a2f7fc3dc281f667b46674e
Mitchell Grams <mitchell.grams@apollomachine.com>
2025-04-17 13:10:00 -0600

    remove unnecessary files

--- modbus_registers.py
+++ modbus_registers.py
@@ -1,8 +1,5 @@
-from IPG_control import set_IPG_callback, set_spindle_state_callback, set_spindle_rpm_callback
-from BLC_control import set_BLC_callback
 
 from outputs import set_output_callback
-from coolant_control import set_coolant_callback
 from event_handler import event_callback
 
 from nuts_bolts import enum
@@ -20,11 +17,11 @@ def set_temp_callback(reg_type, address, val):
 def set_keepalive_callback(reg_type, address, val):
     timestamp = time.time()
 
-# def set_spindle_callback(reg_type, address, val):
-#     print('spindle update received')
+def set_spindle_callback(reg_type, address, val):
+    print('spindle update received')
 
-#def set_coolant_callback(reg_type, address, val):
-#    print('coolant pin update received')
+def set_coolant_callback(reg_type, address, val):
+   print('coolant pin update received')
 
 registers = {
     "HREGS": {
@@ -84,13 +81,13 @@ registers = {
             "register": 0x200,
             "len": 1,
             "val": 0,
-            "on_set_cb": set_spindle_state_callback    
+            "on_set_cb": set_spindle_callback    
         },
         "SPINDLE_SET_RPM_REGISTER": {
             "register": 0x201,
             "len": 1,
             "val": 0,
-            "on_set_cb": set_spindle_rpm_callback    
+            "on_set_cb": set_spindle_callback    
         }
     }    
 }
